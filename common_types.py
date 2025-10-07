3783bd59517b4fa544b2933b7603cde9acd3b308
Chariz Bialen <princesschariz.bialen@gmail.com>
2025-10-07 13:44:42 +0800

    Add files via upload

---
+++ common_types.py
@@ -0,0 +1,14 @@
+from enum import Enum, auto
+
+class Player(Enum):
+    P1 = auto()
+    P2 = auto()
+
+class WinConditionType(Enum):
+    NOT_CONNECT_FOUR = auto()
+    TIC_TAC_TOE = auto()
+
+class TokenPhysicsType(Enum):
+    FLOATING = auto()
+    STRONG_GRAVITY = auto()
+    WEAK_GRAVITY = auto()
